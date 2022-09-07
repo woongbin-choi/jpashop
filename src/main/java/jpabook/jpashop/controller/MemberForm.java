59c9ea7f9a8debb49300ef055518f4daa15b4888
woongbin <woongbin96@naver.com>
2022-09-07 09:07:45 +0900

    add MemberForm

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.controller;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotNull
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+}
