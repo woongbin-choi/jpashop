4f160a45750b9241d2e8f309120d94265d7e87ad
woongbin <woongbin96@naver.com>
2022-09-27 09:09:35 +0900

    update OrderSimpleApiController

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 xToOne(ManyToOne, OneToOne)
@@ -21,7 +26,9 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    // 양방향 관계로 인해 끊기지 않고 계속해서 가져와서 오류 걸림
+    // 양방향 관계로 인해 끊기지 않고 계속해서 가져와서 오류 걸려서 Order와 양방향 관계인 Entity들 @Ignore 처리
+
+    // 좋지 않은 방법
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAll(new OrderSearch());
@@ -30,6 +37,39 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    // DTO로 변환
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        // Order -> 결과 2개
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        // Lazy -> 2번 씩 더 조회
+        // N + 1 문제 = 1 + 회원 N + 배송 N = 5
+
+        return result;
+    }
+
+    // v1, v2 둘다 쿼리문을 너무 많이 보내는 문제점이 있음.
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getMember().getAddress(); // Lazy 초기화
+        }
     }
 
 
